cad37773b038984f5473711644fbb00ae64e4a1c
Yasiru Rangana <yrangana@gmail.com>
2024-01-05 02:00:42 +1100

    Setup files added

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ with open("requirements.txt", "r", encoding="utf-8") as _f:
     requirements = [line for line in _f.read().split("\n")]
 
 setup(
-    name="calc",
+    name="spcalc",
     description="Command Line Calculator",
     packages=find_packages(),
     author="Yasiru Rangana",
     entry_points="""
     [console_scripts]
-    calc=src.main:main
+    spcalc=src.main:main
     """,
     install_requires=requirements,
-    version="1.0.0",
+    version="1.1.0",
     url="https://github.com/yrangana/Strategy_Pattern_Calculator",
 )
