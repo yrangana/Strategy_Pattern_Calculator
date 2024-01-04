68cb40b57e0bfbcf21437a49b23bbb7549e37d73
Yasiru <yrangana@gmail.com>
2024-01-04 14:44:57 +1100

    implement calculator class

--- src/calculator.py
+++ src/calculator.py
@@ -1,1 +1,11 @@
 # calculator class
+
+
+class Calculator:
+    def __init__(self, n1, n2, operation):
+        self._n1 = n1
+        self._n2 = n2
+        self._operation = operation
+
+    def execute(self):
+        return self._operation.execute(self._n1, self._n2)
