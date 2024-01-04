881c141d922a5f1d9e150001adb0a46853f8727c
Yasiru <yrangana@gmail.com>
2024-01-04 15:29:53 +1100

    organized test casses

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -7,27 +7,24 @@ from src.divide import Divide
 import pytest
 
 
-def test_calculator_add():
-    calculator = Calculator(1, 2, Add())
-    assert calculator.execute() == 3
+class TestCalculator:
+    def test_calculator_add(self):
+        calculator = Calculator(1, 2, Add())
+        assert calculator.execute() == 3
 
+    def test_calculator_substract(self):
+        calculator = Calculator(1, 2, Substract())
+        assert calculator.execute() == -1
 
-def test_calculator_substract():
-    calculator = Calculator(1, 2, Substract())
-    assert calculator.execute() == -1
+    def test_calculator_multiply(self):
+        calculator = Calculator(1, 2, Multiply())
+        assert calculator.execute() == 2
 
+    def test_calculator_divide(self):
+        calculator = Calculator(1, 2, Divide())
+        assert calculator.execute() == 0.5
 
-def test_calculator_multiply():
-    calculator = Calculator(1, 2, Multiply())
-    assert calculator.execute() == 2
-
-
-def test_calculator_divide():
-    calculator = Calculator(1, 2, Divide())
-    assert calculator.execute() == 0.5
-
-
-def test_calculator_divide_by_zero():
-    calculator = Calculator(1, 0, Divide())
-    with pytest.raises(ZeroDivisionError):
-        calculator.execute()
+    def test_calculator_divide_by_zero(self):
+        calculator = Calculator(1, 0, Divide())
+        with pytest.raises(ZeroDivisionError):
+            calculator.execute()
